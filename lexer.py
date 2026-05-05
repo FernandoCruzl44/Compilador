94a71ca3a92085aad1986bcfe866e5d2d170eec7
FernandoCruzl44 <a01285406@tec.mx>
2026-05-05 17:34:04 -0600

    Validacion de Lexer

--- lexer.py
+++ lexer.py
@@ -52,7 +52,7 @@ tokens = [
     'DLLAVE', #Llave derecha
     'PUNTOYCOMA',
     'DOSPUNTOS',
-    'COMMA'
+    'COMA'
 ] + list(reserved.values())
 
 #Expresiones regulares para tokens simples
@@ -77,16 +77,53 @@ t_ILLAVE = r'\{'
 t_DLLAVE = r'\}'
 t_PUNTOYCOMA = r';'
 t_DOSPUNTOS = r':'
-t_COMMA = r','
+t_COMA = r','
 
+t_ignore = ' \t'  # Ignorar espacios y tabulaciones
+
+#Funciona para que cada vez que se encuentra un salto de linea, se incremente el numero de linea del lexer
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+
+#Funcion para manejar errores lexicos, pero funciona en conjunto con el t_newline
+#Ya que cuando se encuentra un token que no es valido lo marca pero el t_newline con su incremento
+# de linea hace que el error se marque en la linea correcta, si no se tuviera el t_newline el error se marcaria en la linea 1 siempre
+def t_error(t):
+    print(f"Caracter ilegal '{t.value[0]}' en la línea {t.lexer.lineno}")
+    t.lexer.skip(1)
 
 def t_ID(t):
     r'[a-zA-Z][a-zA-Z0-9]*'
     t.type = reserved.get(t.value, 'ID')  # Verificar si es una palabra reservada
     return t
 
+
 def t_CTE(t):
     r'\d+'
     t.value = int(t.value)
     return t
+
+
+lexer = lex.lex()
+
+# if __name__ == '__main__':
+#     data = '''
+#     program main {
+#         var a,b,x,y,i : int;
+#         begin
+#             x := 1;
+#             for (i := 1; i < n; i++) {
+#                 x := x * i;
+#             }
+#             writeln(x);
+#         end
+#     }
+#     '''
+
+#     lexer.input(data)
+
+#     for tok in lexer:
+#         print(tok)
 
