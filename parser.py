b369d660dbcd22ea9191ca00a45c313b9652ba21
FernandoCruzl44 <a01285406@tec.mx>
2026-05-05 18:33:44 -0600

    Avances parser

---
+++ parser.py
@@ -0,0 +1,82 @@
+import ply.yacc as yacc
+from lexer import tokens
+
+
+# P -> Programa
+def p_programa(p):
+    '''
+        PROGRAM ID ILLAVE variables bloque DLLAVE
+    '''
+
+# V -> Variables
+def p_V_variables(p):
+    '''
+        variables : VAR ids DOSPUNTOS TypeOf PUNTOYCOMA | empty
+    '''
+
+# ids -> Identificadores
+def p_ids(p):
+    '''
+        ids : ID | ids COMA ID
+    '''
+
+# TypeOf -> Tipo de dato
+def p_TypeOf(p):
+    '''
+        TypeOf : INT | FLOAT | BOOL
+    '''
+
+# B -> Bloque
+def p_B_bloque(p):
+    '''
+        bloque : BEGIN PUNTOYCOMA estatutos END PUNTOYCOMA
+    '''
+
+# S -> Estatutos
+def p_S_estatuto(p):
+    '''
+        estatuto: asignar | if | while | for | writeln
+    '''
+
+def p_estatutos(p):
+    '''
+        estatutos : estatuto estatutos | estatuto
+    '''
+
+# A -> Estatuto Asignacion
+def p_A_asignacion(p):
+    '''
+        asignar : ID ASSIGNAR expresion PUNTOYCOMA
+    '''
+
+# W -> Estatuto Writeln
+def p_W_writeln(p):
+    '''
+        writeln : WRITELN IPAREN valor DPAREN PUNTOYCOMA
+    '''
+
+def p_Val_valor(p):
+    '''
+        valor : CTE | ID
+    '''
+
+# IF -> Estatuto if
+def p_IF_if(p):
+    '''
+        if : IF IPAREN expresion DPAREN THEN expresion ELSE estatutos | IF IPAREN expresion DPAREN THEN expresion
+    '''
+
+# WHILE -> Estatuto while
+def p_WHILE_while(p):
+    '''
+        while : WHILE IPAREN expresion DPAREN DO ILLAVE estatutos DLLAVE PUNTOYCOMA
+    '''
+
+# FOR -> Estatuto for
+def p_FOR_for(p):
+    '''
+        for : FOR IPAREN asignar PUNTOYCOMA expresion PUNTOYCOMA expresion DPAREN ILLAVE expresion DLLAVE
+    '''
+
+# E -> Expresion
+def p_E_expresion(p):
